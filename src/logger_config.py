657b85708c14ac5076b2ed33e81b3adf5b3d735f
DhanaCorredor <corredorsadhana@gmail.com>
2026-06-14 23:44:39 +0200

    chore: remove summarizer and logger skeletons (defer HU-6/HU-7)

--- src/logger_config.py
+++
@@ -1,8 +0,0 @@
-"""Configuracion central de logs (HU-7)."""
-
-import logging
-
-
-def configurar_logger(ruta: str = "logs/app.log") -> logging.Logger:
-    """Devuelve un logger que escribe en 'ruta' con fecha y nivel."""
-    raise NotImplementedError
